3d52a66734f944565bc18f234d101a713e9e52af
Moritz Ott <moritz-benjamin.ott@web.de>
2023-01-27 19:45:10 +0100

    Add column-container

---
+++ src/app/components/column-container/column-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-column-container',
+  templateUrl: './column-container.component.html',
+  styleUrls: ['./column-container.component.css']
+})
+export class ColumnContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
